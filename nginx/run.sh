69680f43318e4c6610d4ae9b2b180441689a6af3
Pedro Miguel <pedro.miguel@live.co.uk>
2021-01-07 10:46:04 +0000

    Add docker to project

---
+++ nginx/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -
+
+set -o errexit
+
+mkdir -p /run/nginx
+
+
+# --- Start Insert ENV to JS bundle ---
+echo "== Inserting env variables =="
+for file in /usr/share/nginx/html/*.js
+do
+  echo "== ENV sub for $file =="
+  sed -i 's,https://api.fake.build.com,'${API_BASE_URL}',g' $file
+  sed -i 's,https://sso-fake.build.com/auth,'${KEYCLOAK_AUTH_URL}',g' $file
+  sed -i 's,fake-client-id,'${KEYCLOAK_CLIENT_ID}',g' $file
+  sed -i 's,fake-realm,'${KEYCLOAK_REALM}',g' $file
+done
+echo "== Finished ENV sub =="
+# --- End Insert ENV to JS bundle ---
+
+
+# config file takes precedence
+if [[ -f ${NGINX_CONFIG_FILE} ]]; then
+  echo "== Starting nginx using a config file =="
+  nginx -g 'daemon off;' -c ${NGINX_CONFIG_FILE}
+elif [[ -n ${NGINX_CONFIG} ]]; then
+  echo "== Starting nginx using a config variable =="
+  cp -f <(echo "${NGINX_CONFIG}") /etc/nginx/nginx.conf
+  nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
+else
+  echo "== [error] Please set NGINX_CONFIG_FILE or NGINX_CONFIG variable =="
+  exit 1
+fi
+
