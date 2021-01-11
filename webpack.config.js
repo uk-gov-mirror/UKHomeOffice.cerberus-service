fd85cbc6df4440fa547e22f608f2ff907ef864b4
Pedro Miguel <pedro.miguel@live.co.uk>
2021-01-11 14:48:44 +0000

    Update webpack to use the Govuk design system

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 
 module.exports = {
+  entry: ['./src/', './src/assets/styles/main.scss'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -18,9 +20,27 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/styles/[name].css',
+            },
+          },
+          'sass-loader',
+        ],
+      },
     ],
   },
   plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'node_modules/govuk-frontend/govuk/all.js', to: 'javascript/all.js' },
+        { from: 'node_modules/govuk-frontend/govuk/assets', to: 'assets' }
+      ],
+    }),
     new HtmlWebpackPlugin({template: './src/index.html'}),
   ],
 };
