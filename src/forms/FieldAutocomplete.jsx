46f305989eb60e437f436d56ce00a7740103e542
Rafal Enden <git@enden.com>
2021-02-04 13:59:36 +0000

    Add autocomplete component

---
+++ src/forms/FieldAutocomplete.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import useField from './useField';
+import FormGroup from '../govuk/FormGroup';
+import Autocomplete from '../govuk/Autocomplete';
+
+const FieldAutocomplete = ({
+  id, name, label, hint, defaultValue = '', validate, required, formGroup = {}, ...props
+}) => {
+  const {
+    value, error, setFieldValue,
+  } = useField({
+    name,
+    validate,
+    required,
+    defaultValue,
+  });
+
+  const inputId = id || `field-${name}`;
+
+  return (
+    <FormGroup inputId={inputId} hint={hint} label={label} errorMessage={error} {...formGroup}>
+      <Autocomplete
+        inputId={inputId}
+        aria-label={label}
+        onChange={(newValue) => setFieldValue(name, newValue)}
+        error={error}
+        value={value}
+        {...props}
+      />
+    </FormGroup>
+  );
+};
+
+export default FieldAutocomplete;
---
+++ src/govuk/Autocomplete.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Select, { components as comps } from 'react-select';
+import AsyncSelect from 'react-select/async';
+import classNames from 'classnames';
+
+import './__assets__/Autocomplete.scss';
+
+const CustomOption = ({ data: { label }, ...props }) => (
+  <comps.Option {...props}>{label}</comps.Option>
+);
+
+export const filterOption = ({ label = '' }, query) => label.toLowerCase().includes(query.toLowerCase());
+
+const Autocomplete = ({
+  options, components, error, ...props
+}) => {
+  const customisedProps = {
+    className: classNames('govuk-autocomplete-container', { 'govuk-autocomplete-container--error': error }),
+    classNamePrefix: 'govuk-autocomplete',
+    components: { CustomOption },
+    noOptionsMessage: () => 'No options found',
+    filterOption,
+    ...props,
+  };
+
+  return options ? (
+    <Select
+      options={options}
+      {...customisedProps}
+    />
+  ) : (
+    <AsyncSelect
+      {...customisedProps}
+    />
+  );
+};
+
+export default Autocomplete;
