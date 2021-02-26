a5cf603621e26ff245634c211be48ff7658b36d1
Rafal Enden <git@enden.com>
2021-02-26 12:44:03 +0000

    Render basic version of target sheet form from form builder

---
+++ src/utils/axiosInstance.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useKeycloak } from './keycloak';
+
+const useAxiosInstance = () => {
+  const keycloak = useKeycloak();
+  const [axiosInstance, setAxiosInstance] = useState({});
+
+  useEffect(() => {
+    const instance = axios.create({
+      baseURL: '/',
+      headers: {
+        Authorization: keycloak ? `Bearer ${keycloak.token}` : undefined,
+      },
+    });
+
+    setAxiosInstance({ instance });
+
+    return () => {
+      setAxiosInstance({});
+    };
+  }, [keycloak?.token]);
+
+  return axiosInstance.instance;
+};
+
+export default useAxiosInstance;
