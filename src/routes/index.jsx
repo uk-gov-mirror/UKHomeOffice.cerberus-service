cf27acef71086f1831060e0eb17779c8359f2496
JenniKate Wallace <jennikate@gmail.com>
2021-03-19 13:43:55 +0000

    Remove homepage and redirect / to /tasks

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Redirect, Route } from 'react-router-dom';
 import { initAll } from 'govuk-frontend';
 
 import { useKeycloak } from '../utils/keycloak';
 import Layout from '../components/Layout';
-import HomePage from './HomePage';
 import TaskListPage from './TaskListPage';
 import TaskDetailsPage from './TaskDetailsPage';
 import IssueTargetPage from './IssueTargetPage';
@@ -22,7 +21,7 @@ const AppRouter = () => {
 
   return (
     <BrowserRouter>
-      <Route path="/" exact><Layout><HomePage /></Layout></Route>
+      <Route path="/" exact><Redirect to="/tasks" /></Route>
       <Route path="/tasks" exact><Layout><TaskListPage /></Layout></Route>
       <Route path="/tasks/:taskId" exact>
         <Layout beforeMain={<Link className="govuk-back-link" to="/tasks">Back to task list</Link>}>
