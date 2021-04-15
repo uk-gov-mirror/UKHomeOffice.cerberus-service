3635cdbbc9c2b785f05f71ba3bffb79e1efa5b78
Jonathan Garius <gariusjonathan@hotmail.co.uk>
2021-04-15 11:02:05 +0100

    Add useInterval for token refresh

--- src/utils/keycloak.jsx
+++ src/utils/keycloak.jsx
@@ -2,6 +2,7 @@ import React, {
   createContext, useState, useContext, useEffect,
 } from 'react';
 import Keycloak from 'keycloak-js';
+import { useInterval } from 'react-use';
 
 import config from '../config';
 
@@ -9,13 +10,23 @@ const KeycloakContext = createContext();
 
 const KeycloakProvider = ({ children }) => {
   const [keycloak, setKeycloak] = useState(null);
+  const [timeToExpire, setTimeToExpire] = useState(null);
 
   const keycloakInstance = Keycloak(config.keycloak.clientConfig);
+
+  useInterval(() => {
+    keycloak
+      .updateToken()
+      .catch(() => {
+        keycloak.logout();
+      });
+  }, keycloak ? timeToExpire : null);
 
   useEffect(() => {
     keycloakInstance.init(config.keycloak.initOptions).then((authenticated) => {
       if (authenticated) {
         setKeycloak(keycloakInstance);
+        setTimeToExpire(new Date(keycloakInstance.tokenParsed.exp * 1000) - new Date());
       } else {
         keycloakInstance.login();
       }
